41a8f0a32d56101c88703b78a2b146e3101158a6
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-02 05:51:29 -0500

    adding static files

--- app.ts
+++ app.ts
@@ -1,17 +1,20 @@
-import express , {Application} from 'express'
+import express , {Application , Request , Response } from 'express'
 import * as http from 'http'
 import helmet from 'helmet'
 import {Socket , Server } from 'socket.io'
-
+import * as path from 'path'
 const app : Application = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(helmet());
+app.use(express.static(path.join(__dirname , 'public')))
+
 // Socket 
+
 io.on("connection", async ( socket : Socket) =>{
    getSocketId :   console.log(`${socket.id} connected `);
-   
+
 });
 
 
