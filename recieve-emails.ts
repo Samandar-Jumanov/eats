099a9de75b26fe67af5029fe374682ff0ebf20ca
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-11 12:38:06 -0500

    adding recieve emails

--- recieve-emails.ts
+++ recieve-emails.ts
@@ -1,12 +1,13 @@
-import * as dotenv from 'dotenv';
+import {config} from 'dotenv';
 import  Imap from 'node-imap'
 
-dotenv.config();
+config();
 
-const email : string = process.env.EMAIL;
-const password : string = process.env.EMAIL_PASSWORD ;
-const host : string = process.env.EMAIL_HOST ;
-const port : number = process.env.PORT;
+
+const email : string = process.env.EMAIL || ''
+const password : string = process.env.EMAIL_PASSWORD || ''
+const host : string = process.env.EMAIL_HOST ||''
+const port : number = Number(process.env.PORT)
 
 
 const imap = new Imap({
@@ -18,17 +19,47 @@ const imap = new Imap({
 });
 
 
-imap.once('ready', ()=>{
-      console.log("Imap connnected ")
-});
-
-
 
 (async function connectImap(){
     try {
-    await imap.connect();
+        await imap.connect();
      console.log('Imap connected')
     } catch (error : any  ) {
           console.log(` Error imap : ${error.message}`)
     }    
 })();
+
+
+
+imap.once('ready', async () => {
+    await imap.openBox('INBOX', true, (err) => {
+      if (err) throw err;
+  
+      // Function to fetch emails
+      const fetchEmails = async () => {
+        const fetchOptions = { bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)'] };
+        const fetch = imap.seq.fetch('1:*', fetchOptions);
+        fetch.on('message', (msg, seqno) => {
+          console.log(`Message seqno #${seqno}`);
+          msg.on('body', (stream, info) => {
+            let buffer = '';
+            stream.on('data', (chunk) => {
+              buffer += chunk.toString('utf8');
+            });
+            stream.on('end', () => console.log(buffer));
+          });
+        });
+  
+        fetch.once('end', () => {
+          console.log('No more tasks');
+        });
+      };
+  
+     
+      setInterval(fetchEmails, 1000); // 1 second
+    });
+  });
+  
+
+
+
