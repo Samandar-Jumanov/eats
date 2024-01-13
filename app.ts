72d6ca5cf6998bda71ea92c71c7766d4697fee8c
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-13 12:07:55 -0500

    fixing bugs

--- app.ts
+++ app.ts
@@ -1,44 +1,18 @@
-import express , {Application  , Request , Response  } from 'express'
+import express , {Application } from 'express'
 import * as http from 'http';
 import helmet from 'helmet';
 import  cors from 'cors'
 import  morgan  from 'morgan'
 import socket from './socket';
 import { Server } from 'socket.io';
-
-import DataHash from "./hash-data";
-import init from "./recieve-emails";
-
+// import { fetchEmails} from './recieve-emails'
 const app : Application = express();
 const server = http.createServer(app)
 app.use(express.json());
 
 const io = new Server(server);
 
-const hashData = new DataHash();
-
-async function checkData() {
-
-  const res = await init();
-
-  if (res) {
-    let searchKey = res[0];
-    const isFound = hashData.search(searchKey);
-      if(isFound !== true ){
-          hashData.saveData(res[0] , res[1])
-      }
-  }
-
-    return hashData.data;
-}
-
-  const res = init();
-  res.then(res =>{
-       console.log(res)
-  }).catch(err =>{
-         console.log(err?.message)
-  })
-
+// init();
 
 
 app.use(cors({
@@ -50,26 +24,14 @@ app.use(cors({
 app.use(helmet());
 app.use(morgan('dev'));
 
-checkData()
-app.get('/', async ( request  : Request , response : Response  )=>{
-     
-          const data =  hashData.data 
-            response.json({
-              data 
-        })
-})
-
 // Socket 
 socket(io);
 
-// init()
+// fetchEmails()
+
+
 server.listen(3001, ()=> {
   console.log("Server is working on 3001 ") 
 } )
 
-
-// setInterval( async ()=>{
-//   await checkData();
-    
-// } , 1000);
 
--- saveData.ts
+++ saveData.ts
@@ -1,36 +1,36 @@
-import DataHash from "./hash-data";
-import init from "./recieve-emails";
+// import DataHash from "./hash-data";
+// import init from "./recieve-emails";
 
-const hashData = new DataHash();
+// const hashData = new DataHash();
 
-async function checkData ()  : Promise<null | string[]> {
-  // check if there is a new data 
-  // if there return data or return false 
-    const res : string[] = await init();
-    let searchKey = res[0]
-    const isFound =   hashData.search(searchKey);
+// async function checkData ()  : Promise<null | string[]> {
+//   // check if there is a new data 
+//   // if there return data or return false 
+//     const res : string[] = await init();
+//     let searchKey = res[0]
+//     const isFound =   hashData.search(searchKey);
     
-    return isFound === true ? res :  null
-}
+//     return isFound === true ? res :  null
+// }
 
 
-async function saveData() : Promise<void | null> {
-       // check if there any data 
-       // if there save it 
-       // or return null 
-       const result : string[] = await  checkData();
-       if(result === null ){
-             hashData.saveData(result[0] , result[1])
-             return 
-       }
+// async function saveData() : Promise<void | null> {
+//        // check if there any data 
+//        // if there save it 
+//        // or return null 
+//        const result : string[] = await  checkData();
+//        if(result === null ){
+//              hashData.saveData(result[0] , result[1])
+//              return 
+//        }
 
-       return null;
-}
-
-
+//        return null;
+// }
 
 
-export default hashData
+
+
+// export default hashData
 
 
 
