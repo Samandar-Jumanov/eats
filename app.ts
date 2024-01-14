2e6745761d1d8e33a5adfdfb55593c6d3591c8bd
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-14 12:15:09 -0500

    adding events to connections

--- app.ts
+++ app.ts
@@ -1,27 +1,55 @@
-import express , {Application} from 'express';
+import express , {Application , Request , Response } from 'express';
 import morgan from 'morgan';
-import connectRedis from './utils/redisClient';
-
+import http from 'http';
+import { Server , Socket  } from 'socket.io'
+import cors from 'cors'
 
 const app :Application = express()
-
 app.use(morgan('dev'))
+app.use(express.json());
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection' , ( socket:Socket   ) =>{
+    console.log(socket.id);
+
+    socket.on("offer" , (data : string ) =>{
+          console.log(data)
+    })
+    
+    socket.on("message" , ( data : string ) =>{
+        socket.broadcast.emit("offer" , data )
+     })
+
+    socket.on('answer', (data) => {
+        socket.broadcast.emit('answer', data);
+    });
+    
+    socket.on('ice-candidate', (data) => {
+        socket.broadcast.emit('ice-candidate', data);
+    });
+    
+        socket.on('disconnect' , () =>{
+             console.log(`User disconnected ${socket.id}`)
+        });
+});
 
 
-connectRedis().then((res )=>{
-       console.log(res)
-}).then((err : any )=>{
-      console.log(err.message)
+
+app.get('/' , ( request : Request , response : Response ) => {
+            response.json({
+                message :' Hello world '
+            })
 })
 
 
-app.get('/all' , )
 
-
-app.listen(3001 , ()=>{
-        console.log("Http server started ")
+app.listen(3001, () =>{
+          console.log("Server is running ")
 })
-
 
 
 
