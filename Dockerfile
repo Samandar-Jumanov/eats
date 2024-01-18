256cbdaf44aa0715e48057094325bb45c93975ee
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-18 12:39:22 -0500

    fixng mongo

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:21 
+FROM node:21
 
 WORKDIR /
 
@@ -6,7 +6,7 @@ COPY package.json ./
 
 RUN npm install
 
-COPY . ./app
+COPY . .
 
 EXPOSE 3001
 
--- utils/connect-mongo.ts
+++ utils/connect-mongo.ts
@@ -1,18 +1,26 @@
-import { connect } from 'mongoose';
+import  mongoose , { connect } from 'mongoose';
 import { config } from 'dotenv';
 
 config();
 
 const connectionUrl  : string = String(process.env.MONGO_URL)
-
-
 async function mongoDBConnection() {
     try {
-        await connect(connectionUrl)
+        await connect(connectionUrl , {
+          
+        });
+
         console.log("Connected to mongo")
     } catch (error : any ) {
           console.log(error.message)
     }
 }
+const dbConnection  = mongoose.connection;
+
+dbConnection.on("error" , ( error : any ) =>{
+      console.log(error.message)
+});
+
+
 
 export default mongoDBConnection
