6defb96156db863e2b5800d9544b8df00c0b6a31
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-22 07:12:03 -0500

    adding pages

--- app.ts
+++ app.ts
@@ -1,20 +1,15 @@
-import express, { Application, Request, Response } from 'express';
-import morgan from 'morgan';
+import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
 import mongoDBConnection from './utils/connect-mongo';
 import { sockerServer } from './controller/socket-server';
-import path from 'path';
-import { ExpressPeerServer } from 'peer';
+// import path from 'path';
+// import { ExpressPeerServer } from 'peer';
 
-const app: Application = express();
+const app = express();
 const server = http.createServer(app);
-const peerServer = ExpressPeerServer(server , {
-  path : '/peer'
-});
-
-const io: Server = new Server(server, {
+const io = new Server(server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
@@ -22,31 +17,26 @@ const io: Server = new Server(server, {
   },
 });
 
-// Configure EJS as the view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-// Middleware
+
+
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
 
-app.use('/', peerServer); // Use the PeerServer middleware for the /peer path
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
-// Routes
-app.get('/', (request: Request, response: Response) => {
-  response.render('client');
-});
+
+
 
 // MongoDB and Socket.io setup
 mongoDBConnection();
 sockerServer(io);
 
-// Start the server
+app.get('/', (req, res) => {
+  res.render('client');
+});
+
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
