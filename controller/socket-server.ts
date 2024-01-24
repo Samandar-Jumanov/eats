124e59322769030043e7d2d22c1efa2b55363bc4
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-24 05:30:47 -0500

    fiixng bugs

--- controller/socket-server.ts
+++ controller/socket-server.ts
@@ -1,22 +1,44 @@
 import { Socket , Server } from 'socket.io'
 import Room from '../controller/room'
 import RoomType from '../interface';
+import SimplePeer  from 'simple-peer';
+
 
 export const socketServer = (io: Server) => {
     const room = new Room();
     
     io.on('connection', (socket: Socket) => {
+      const peer  = new SimplePeer({ initiator :  true });
+      // Peer connections 
+      peer.on("signal" , ( data ) =>{
+          socket.emit("signal", data);
+      });
+      socket.on("signal" , ( data : any  ) =>{
+           peer.signal(data)
+      });
+
+      peer.on("connect" , (  ) =>{
+            peer.send(" Wassup ! ")
+      });
+
+
+      peer.on("data" , ( data ) =>{
+            console.log("Recieved data " , data)
+      });
+      
        socket.emit("newUser" , socket.id)
-      socket.on('create-room', (data) => {
-        room.createRoom(data.roomName, data.adminName, socket.id);
+       socket.on('create-room', (data) => {
+       room.createRoom(data.roomName, data.adminName, socket.id);
       });
   
       socket.emit('rooms-available', room.rooms);
+
   /// Join room process 
 
-   socket.on('join-room', ( data ) => {
-    room.joinToRoom(data.roomName, data.userName);
-   });
+      socket.on('join-room', ( data ) => {
+       room.joinToRoom(data.roomName, data.userName);
+       });
+      
     
       
       socket.emit('leave-room', (data: RoomType) => {
