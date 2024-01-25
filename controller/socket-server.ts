c3ea35850e4689f95a9af61bf3646b7bde2a0d38
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-24 23:46:04 -0500

    ficing bugs

--- controller/socket-server.ts
+++ controller/socket-server.ts
@@ -16,23 +16,30 @@ export const socketServer = (io: Server) => {
 
     socket.on('leave-room', (data) => { // Leave room 
       room.leaveRoom(data.roomName , data.userName );
+      console.log(data , ' Left')
     });
 
-    socket.on('room', (roomName: string) => {
-      const foundRoom = room.search(roomName);
-      socket.emit('room-info', foundRoom);
-      console.log("Room info sent ")
-    }); // room info 
-
+  
 
     socket.emit("rooms-available" , room.rooms ); // rooms available 
 
 
     socket.on("create-room" , (data ) =>{
-         room.createRoom(data.roomName  , data.userName);
+         const  isRoomCreated =  room.createRoom(data.roomName  , data.userName);
+         console.log(isRoomCreated);
          socket.emit("answer" , true );
          console.log("Room created  by : " ,  socket.id)
     }) // create room
+
+    socket.on('room', (roomName: string) => {
+      const foundRoom = room.search(roomName);
+     
+      socket.emit('room-info', foundRoom);
+      console.log("Room info sent to :  " , foundRoom?.admin);
+      console.log(foundRoom);
+      
+    }); // room info 
+
 
     socket.on('disconnect', () => {
       room.removeUserOnDisconnect(socket.id);
