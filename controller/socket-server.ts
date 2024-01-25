1d02d8245bb4b41b64fd9ce63c89db7f2df82f32
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-25 00:42:14 -0500

    fiixng bugs

--- controller/socket-server.ts
+++ controller/socket-server.ts
@@ -11,12 +11,17 @@ export const socketServer = (io: Server) => {
     socket.to(socket.id).emit("id" , socket.id);
     socket.emit("user-connected" , socket.id)
 
-    socket.on("create-room" , (data ) =>{
-      const  isRoomCreated =  room.createRoom(data.roomName  , data.userName);
-      console.log(isRoomCreated);
-      socket.emit("answer" , true );
-      console.log("Room created  by : " ,  socket.id)
- }) // create room
+  socket.on("create-room", (data) => {
+  const isRoomCreated = room.createRoom(data.roomName, data.userName);
+  if (isRoomCreated === true) {
+      const foundRoom = room.search(data.roomName);
+      socket.emit("room-info", foundRoom);
+  } else {
+      socket.emit("answer", false); 
+  }
+  console.log("Room created by : ", socket.id);
+});
+
 
 
  socket.on('leave-room', (data) => { // Leave room 
